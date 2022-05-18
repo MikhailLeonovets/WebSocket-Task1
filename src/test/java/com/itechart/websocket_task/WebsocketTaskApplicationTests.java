46ebda240a9abcf7ad1f6704dc6eb7620ae6bf4c
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-05-18 16:41:44 +0300

    application structure created

---
+++ src/test/java/com/itechart/websocket_task/WebsocketTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechart.websocket_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
